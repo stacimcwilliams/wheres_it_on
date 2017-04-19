841d564736a7c35ed8fecb7aa4408d7c4f66adc5
Staci McWilliams <stacinicole86@gmail.com>
2017-04-19 10:42:21 -0600

    testing

--- src/tests/actions/index.test.js
+++ src/tests/actions/index.test.js
@@ -1,5 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import * as actions from '../../actions/index.js';
+import fetchMock from 'fetch-mock';
+
 
 const store = configureMockStore()()
 
@@ -7,10 +9,10 @@ describe('actions', () => {
 
 const mockData = {
   results: [{
-    poster_120x171: "http://static-api.guidebox.com/thumbnails_movies_small/27849-575402165-6600297903-9267753171-small-120x171.jpg"
-    title: "Harry Potter and the Chamber of Secrets"
-    rating: "PG"
-    release_date: "2002-11-13"
+    poster_120x171: "http://static-api.guidebox.com/thumbnails_movies_small/27849-575402165-6600297903-9267753171-small-120x171.jpg",
+    title: "Harry Potter and the Chamber of Secrets",
+    rating: "PG",
+    release_date: "2002-11-13",
     overview: "The next installment in the Harry Potter series finds young wizard Harry Potter (Daniel Radcliffe) and his friends Ron Weasley (Rupert Grint) and Hermione Granger (Emma Watson) facing new challenges during their second year at Hogwarts School of Witchcraft and Wizardry as they try to uncover a dark force that is terrorizing the school."
   }]
 }
@@ -19,4 +21,45 @@ const mockData = {
     expect(fetchMock.calls().unmatched).toEqual([])
     fetchMock.restore()
   })
+
+
+it('getTV', () => {
+  let expectedAction = { type: 'GET_TV', shows: mockData.results}
+
+  store.dispatch(actions.getTv(mockData.results))
+  let createdAction = store.getActions()
+
+  expect(createdAction.length).toEqual(1)
+  expect(createdAction[0]).toEqual(expectedAction)
+})
+
+it('searchResults', () => {
+  let expectedAction = { type: 'GET_TV', shows: mockData.results}
+
+  store.dispatch(actions.searchResults(mockData.results))
+  let createdAction = store.getActions()
+
+  expect(createdAction.length).toEqual(2)
+  expect(createdAction[0]).toEqual(expectedAction)
+})
+
+it('singleResult', () => {
+  let expectedAction = { type: 'GET_SINGLE', shows: mockData.results}
+
+  store.dispatch(actions.searchResults(mockData.results))
+  let createdAction = store.getActions()
+
+  expect(createdAction.length).toEqual(1)
+  expect(createdAction[0]).toEqual(expectedAction)
+})
+
+it('it creates getTV when initiating fetchRandomTv action', () => {
+  let expectedAction = { type: 'fetchRandomTv', shows: mockData.results}
+
+  store.dispatch(fetchRandomTv(mockData.results))
+  let createdAction = store.getActions()
+
+  expect(createdAction.length).toEqual(1)
+  expect(createdAction[0]).toEqual(expectedAction)
+})
 })
